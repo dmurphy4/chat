b6f4f8463e859ea68b85d6d257a015b17dbe6f1b
dmurphy4 <murphy.dallin@gmail.com>
2017-11-03 04:59:33 +0000

    Final push

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message',msg);
-  });
+  });  
 });
 
 
